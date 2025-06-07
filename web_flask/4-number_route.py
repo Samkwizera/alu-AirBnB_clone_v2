a0522ae707a314665ce1d01b85d535002028efba
samkwizera <s.ihimbazwe@alustudent.com>
2025-06-07 13:22:34 -0700

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
+"""Starts a Flask web application with multiple routes and variable rules"""
 
-"""Script that starts a Flask web application"""
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Comment"""
+@app.route("/", strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Comment"""
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def text_route(text):
-    """Comment"""
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C ' followed by text with underscores replaced by spaces"""
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def text_route_python(text="is cool"):
-    """Comment"""
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python ' followed by text (default 'is cool')"""
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def num_route(n):
-    """Comment"""
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays '<n> is a number' only if n is an integer"""
     return "{} is a number".format(n)
 
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
